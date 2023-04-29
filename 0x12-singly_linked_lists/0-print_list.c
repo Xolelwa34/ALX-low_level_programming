60bf2590492670e4d8aabcf7d6daa532f92165b8
Xolelwa <ciliaswekile@gmail.com>
2023-04-29 03:52:49 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Function that prints the elements of the linked list
+ * @h:The pointer to the list-t list to print
+ * Return: the number of printed nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+	if (h->str == NULL)
+	printf("[0] (nil)\n");
+	else
+	printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+	num++;
+	}
+
+	return (num);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+0x12. C - Singly linked lists
+A task that contains data and a pointer to the next mode, of which it assists in mantaining the list structure.
