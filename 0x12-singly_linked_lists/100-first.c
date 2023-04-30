6938b6cd1da4d95ea1527a75cbeb374bdbad61a4
Xolelwa <ciliaswekile@gmail.com>
2023-04-30 09:18:57 -0700

    The hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *lint - It prints a string before the
+ * function main get's executed.
+ */
+
+void lint(void)
+{
+	printf("(You're beat! and yet, you must allow.)\n");
+	printf("Upon my bach, I bore my house!\n");
+}
