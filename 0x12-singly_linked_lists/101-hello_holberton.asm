e3cb36e123c26916aea6746e73be9bcdf504ea57
Xolelwa <ciliaswekile@gmail.com>
2023-05-01 01:17:34 -0700

    Real programmers can write assembly code in any language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+global  main
+section .text
+main:
+        mov     rax, 1
+        mov     rdi, 1
+        mov     rsi, 2
+        mov     rdx, 17
+	syscall
+
+        mov     rax, 60
+        xor     rdi, rdi
+	syscall
+
+        section .data
+
+format: db      "Hello, Holberton", 10,0
