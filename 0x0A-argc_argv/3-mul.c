f2695be54db07ddf890b45b9b82cb9e433d7b1a8
Xolelwa34 <ciliaswekile@gmail.com>
2023-09-29 07:20:30 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,9 +3,8 @@
 
 /**
  * main - Entry point
- *
- * @argc: incoming argument
- * @argv: incoming argument
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
