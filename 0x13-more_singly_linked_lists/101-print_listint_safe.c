4aa9da25d5a2b325cf163e09ec198300f96bf2c4
Xolelwa34 <ciliaswekile@gmail.com>
2023-10-24 11:01:41 -0700

    Free safe version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,10 +2,25 @@
 /**
   * print_listint_safe -prints elementsof linked list
   * @head: pointer to the head of the list
-  * Return: null
+  *
+  * Return: addredd of the head
   */
 size_t print_listint_safe(const listint_t *head)
 {
-	head++;
+	if (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head->next < head)
+		{
+			return (1 + print_listint_safe(head->next));
+		}
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			return (1);
+		}
+
+	}
 	return (0);
 }
+
