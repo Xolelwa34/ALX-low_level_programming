1279ef1b14f99c1e58a01eca94e3f9af0c44ade5
Xolelwa34 <ciliaswekile@gmail.com>
2024-05-22 11:37:50 -0700

    jump list

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,65 +1,49 @@
 #include "search_algos.h"
 
 /**
- * print_array - print an array
- * @array: pointer to array
- * @l: left index
- * @r: right index
+ * _advanced_binary - searches for value in array of integers
+ * @array: pointer to array of integers
+ * @size: size of an array
+ * @value: value to search for
+ *
+ * Return: the index containing value, or -1 if not found 
  */
-void print_array(int *array, int l, int r)
+int *_advanced_binary(int *array, size_t size, int value)
 {
-	int i;
+	size_t i = 0;
 
-	for (i = l; i <= r; i++)
+	if (!size || !array)
+		return (NULL);
+	for (printf("Searching in array: "); i < size; i++)
+		printf("%d%s", array[i], i + 1 == size ? "\n" : ", ");
+
+	i = (size - 1) / 2;
+	if (array[i] == value)
 	{
-		printf("%d", array[i]);
-		if (i < r)
-			printf(", ");
+		if (i)
+			return (_advanced_binary(array, i + 1, value));
+		return (array + i);
 	}
-	printf("\n");
+	else if (array[i] > value)
+		return (_advanced_binary(array, i + 1, value));
+	else
+		return (_advanced_binary(array + i + 1, size - i - 1, value));
 }
 
-int binary_recurtion(int *array, int left, int right, int value);
-
 /**
-* advanced_binary - searches for a value in an array of integers
-* using the advanced binary search algorithm
-* @array: pointer to an array
-* @size: size of array
-* @value: value that to search for in an array
-* Return: return the index value, or -1 if not found
-*/
+ * advanced_binary - searches advanced binary value in array of integers
+ * @array: arrayof integer
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: the index containing value, or -1 if not found
+ */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (!array || size == 0)
+	int *a = _advanced_binary(array, size, value);
+
+	if (!a)
 		return (-1);
-	return (binary_recurtion(array, 0, (int) size - 1, value));
-}
-
-/**
-* binary_recurtion - function that searches for a value in an array of integers
-* @array: pointer to the array of integers
-* @left: first array
-* @right: second of array
-* @value: value to search for in array
-* Return: return the index of value, or -1 if not found
-*/
-
-int binary_recurtion(int *array, int left, int right, int value)
-{
-	int m = (left + right) / 2;
-
-	printf("Searching in array: ");
-	print_array(array, left, right);
-	if (left >= right)
-		return (-1);
-	if (array[m] == value)
-	{
-		if (array[m - 1] != value)
-			return (m);
-		else
-			return (binary_recurtion(array, left--, m, value));
-	} else if (value < array[m])
-		return (binary_recurtion(array, left, m - 1, value));
 	else
-		return (binary_recurtion(array, m + 1, right, value));
+		return (a - array);
+}
